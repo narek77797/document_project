8b9cc1718e8d8774238bd7747945f62dd91a7c9f
jigyar <jigyar@gmail.com>
2023-11-29 01:33:43 +0400

    Test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,195 +1,61 @@
-def getdockertag(){
-    return "${env.GIT_BRANCH}".replace("/",".") + "."+"${env.BUILD_ID}"
+#!/usr/bin/env groovy
+def HTTP_PORT="8090"
+def CONTAINER_TAG="latest"
+def DOCKER_HUB_USER="narek305"
+def CONTAINER_NAME="jenkins-pipeline"
+
+node {
+    stage('Initialize'){
+        def dockerHome = tool 'myDocker'
+        def mavenHome  = tool 'myMaven'
+        env.PATH = "${dockerHome}/bin:${mavenHome}/bin:${env.PATH}"
+    }
+
+    stage('Checkout') {
+        checkout scm
+    }
+
+    stage("Image Prune"){
+        imagePrune(CONTAINER_NAME)
+    }
+
+    stage('Image Build'){
+        imageBuild(CONTAINER_NAME, CONTAINER_TAG)
+    }
+
+//     stage('Push to Docker Registry'){
+//         withCredentials([usernamePassword(credentialsId: 'dockerHubAccount', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+//             pushToImage(CONTAINER_NAME, CONTAINER_TAG, USERNAME, PASSWORD)
+//         }
+//     }
+
+    stage('Run App'){
+        runApp(CONTAINER_NAME, CONTAINER_TAG, DOCKER_HUB_USER, HTTP_PORT)
+    }
+
 }
-pipeline {
-    agent any
-    environment {
-        DOCKER_REGISTRY = "pto/php-deployment"
-        DOCKER_TAG = getdockertag()
-    }
-    stages {
-//НАДО РАСКОММЕНТИРОВАТ
-//         stage('Build') {
-//             steps {
-//                 script {
-//                     echo "install compose.json"
-//                     sh 'composer install --prefer-source'
-//                     sh 'printenv'
-//                     dir('ansible') {
-//                         sh 'ansible-galaxy install -r requirements.yml'
-//                     }
-//                 }
-//             }
-//         }
-//         stage('UnitTest') {
-//             steps {
-//                 script {
-//                     try{
-//                         echo "Running docker-compose"
-//                         sh 'cp .env.example .env'
-//                         sh 'chmod o+w .env'
-//                         sh 'make init'
-//                     }
-//                     catch(Exception e){
-//                         if ( GIT_BRANCH ==~ /.*master|.*hotfix\/.*|.*release\/.*/ )
-//                                             error "Test case failed"
-//                         else
-//                             echo "Skipped test if from personal or feature branch"
-//                     }
-// //НАДО РАСКОММЕНТИРОВАТ
-// //                     try{
-// //                         echo "Running Test cases"
-// //                         sh './vendor/bin/phpunit --colors tests --log-junit reports/junit.xml'
-// //                     }
-// //                     catch(Exception e){
-// //                         if ( GIT_BRANCH ==~ /.*master|.*hotfix\/.*|.*release\/.*/ )
-// //                             error "Test case failed"
-// //                         else
-// //                             echo "Skipped test if from personal or feature branch"
-// //                     }
-// //                     try{
-// //                         echo "Running Test code coverage"
-// //                         sh './vendor/bin/phpunit --coverage-clover reports/codeCoverage.xml'
-// //                     }
-// //                     catch(Exception e){
-// //                         if ( GIT_BRANCH ==~ /.*master|.*hotfix\/.*|.*release\/.*/ )
-// //                             error "Code coverage failed"
-// //                         else
-// //                             echo "Skipped code coverage if from personal or feature branch"
-// //                     }
-//                 }
-//             }
-//         }
-//НАДО РАСКОММЕНТИРОВАТ
-//         stage('CodeAnalysis') {
-//             when {
-//                 expression {
-//                     GIT_BRANCH ==~ /.*master|.*feature\/.*|.*develop|.*hotfix\/.*|.*release\/.*/
-//                 }
-//             }
-//             steps {
-//                 script {
-//                     scannerHome = tool name: 'sonar-scanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-//                 }
-//                 withSonarQubeEnv('sonarqube.io') {
-//                     sh "${scannerHome}/bin/sonar-scanner -Dsonar.branch.name=${GIT_BRANCH} -Dsonar.projectKey=varunpalekar_php-test -Dsonar.organization=varunpalekar-github"
-//                 }
-//             }
-//         }
-//         stage('DockerPush') {
-//             when {
-//                 expression {
-//                     GIT_BRANCH ==~ /.*master|.*release\/.*|.*develop|.*hotfix\/.*/
-//                 }
-//             }
-//             steps {
-//                 script{
-//                     docker.withRegistry('', 'public-docker-hub') {
-//
-//                         def customImage = docker.build("${env.DOCKER_REGISTRY}:${env.DOCKER_TAG}")
-//                         customImage.push()
-//
-//                         if ( GIT_BRANCH ==~ /.*master|.*hotfix\/.*|.*release\/.*/ )
-//                             customImage.push('latest')
-//                     }
-//                 }
-//             }
-//         }
 
-        stage('Build and Deploy Dev') {
-            when {
-                expression {
-                    GIT_BRANCH ==~ /.*master|.*feature\/.*|.*develop|.*hotfix\/.*|.*release\/.*/
-                }
-            }
-            steps {
-                script {
-                    // Build Docker image for development
-                    sh 'docker build -t myapp-dev:latest'
-                    // Deploy development environment (adapt as needed)
-                    sh 'docker run -d --name myapp-dev -p 8080:80 myapp-dev:latest'
-                }
-            }
-        }
-//         stage('Deploy_Dev') {
-//             when {
-//                 expression {
-//                     GIT_BRANCH ==~ /.*develop/
-//                 }
-//             }
-//             steps {
-//                 script{
-//                     echo "Deploy application on developmment environment"
-//                     dir("ansible") {
-//                         ansiblePlaybook installation: 'ansible', inventory: 'hosts-dev', playbook: 'playbook.yml', extraVars: [
-//                             deployment_app_image: "${env.DOCKER_REGISTRY}:${env.DOCKER_TAG}"
-//                         ]
-//                     }
-//                 }
-//
-//                 input message: "Do you want to run migration?"
-//
-//                 script{
-//                     echo "Deploy application on developmment environment"
-//                     dir("ansible") {
-//                         ansiblePlaybook installation: 'ansible', inventory: 'hosts-dev', playbook: 'playbook.yml', tags: 'migration'
-//                     }
-//                 }
-//
-//                 input message: "Do you want to run seeding?"
-//
-//                 script{
-//                     echo "Deploy application on developmment environment"
-//                     dir("ansible") {
-//                         ansiblePlaybook installation: 'ansible', inventory: 'hosts-dev', playbook: 'playbook.yml', tags: 'seeding'
-//                     }
-//                 }
-//             }
-//         }
+def imagePrune(containerName){
+    try {
+        sh "docker image prune -f"
+        sh "docker stop $containerName"
+    } catch(error){}
+}
 
-//         stage('Undeploy_Dev'){
-//             when {
-//                 expression {
-//                     GIT_BRANCH ==~ /.*develop/
-//                 }
-//             }
-//             // timeout(time:5, unit:'DAYS') {
-//             //     input message:'Approve deployment?', submitter: 'it-ops'
-//             // }
-//             steps {
-//                 input message: "Do you want to undeploy DEV?"
-//                 script {
-//                     echo "Undeploy application on developmment environment"
-//                     dir("ansible") {
-//                         ansiblePlaybook installation: 'ansible', inventory: 'hosts-dev', playbook: 'playbook.yml', tags: 'undeploy'
-//                     }
-//                 }
-//             }
-//         }
-//         stage('Deploy_Prod') {
-//             when {
-//                 expression {
-//                     GIT_BRANCH ==~ /.*master|.*release\/.*|.*hotfix\/.*/
-//                 }
-//             }
-//             steps {
-//                 input message: "Do you want to proceed for production deployment?"
-//                 script{
-//                     echo "Deploy application on stage environment"
-//                     dir("ansible") {
-//                         ansiblePlaybook installation: 'ansible', inventory: 'hosts-prod', playbook: 'playbook.yml', credentialsId: 'ansible-hospice-prod'
-//                     }
-//                 }
-//
-//                 input message: "Do you want to proceed for production migration?"
-//                 script{
-//                     echo "Deploy application on stage environment"
-//                     dir("ansible") {
-//                         ansiblePlaybook installation: 'ansible', inventory: 'hosts-prod', playbook: 'playbook.yml', tags: 'migration', credentialsId: 'ansible-hospice-prod'
-//                     }
-//                 }
-//             }
-//         }
+def imageBuild(containerName, tag){
+    sh "docker build -t $containerName:$tag  -t $containerName --pull --no-cache ."
+    echo "Image build complete"
+}
 
-    }
+def pushToImage(containerName, tag, dockerUser, dockerPassword){
+    sh "docker login -u $dockerUser -p $dockerPassword"
+    sh "docker tag $containerName:$tag $dockerUser/$containerName:$tag"
+    sh "docker push $dockerUser/$containerName:$tag"
+    echo "Image push complete"
+}
+
+def runApp(containerName, tag, dockerHubUser, httpPort){
+//     sh "docker pull $dockerHubUser/$containerName"
+    sh "docker run -d --rm -p $httpPort:$httpPort --name $containerName $dockerHubUser/$containerName:$tag"
+    echo "Application started on port: ${httpPort} (http)"
 }
